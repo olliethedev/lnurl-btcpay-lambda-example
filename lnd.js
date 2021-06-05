f6076e1e62dcda3b2f87aa55fafc1d945060c84f
Ollie <3martynov@gmail.com>
2021-06-05 10:59:07 -0400

    build param clean up

--- lnd.js
+++
@@ -1,7 +0,0 @@
-const lnService = require("ln-service");
-
-module.exports = lnService.authenticatedLndGrpc({
-    cert: process.env.LND_CERT,
-    macaroon: process.env.LND_MACAROON,
-    socket: process.env.LND_URL,
-  }).lnd;
